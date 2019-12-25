9ba859320681f4261679c16c99612ffbbfab96ac
wangrunyuan <wangrunyuan@haiwainet.cn>
2019-12-25 15:13:20 +0800

    添加单元测试

--- src/test/java/com/runyuanj/upload/UploadApplicationTests.java
+++ src/test/java/com/runyuanj/upload/UploadApplicationTests.java
@@ -1,13 +1,37 @@
 package com.runyuanj.upload;
 
+import com.runyuanj.upload.service.UploadService;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.util.Assert;
+import org.springframework.web.multipart.MultipartFile;
 
-@SpringBootTest
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.List;
+
+@SpringBootTest(classes = {UploadApplication.class})
 class UploadApplicationTests {
 
+    @Autowired
+    private UploadService uploadService;
+
     @Test
-    void contextLoads() {
+    void testUploadFile() throws Exception {
+        MultipartFile[] files = new MultipartFile[1];
+
+        File file = new File("D:\\usr\\share\\nginx\\html\\runyu\\video\\2019\\12\\25\\2d61adf700b241c5a7cc95c11ab4201c.mp4");
+
+        MockMultipartFile mockMultipartFile = new MockMultipartFile(file.getName(), file.getName(),
+                "video", new FileInputStream(file));
+
+        files[0] = mockMultipartFile;
+        List<String> strings = uploadService.uploadFile(files);
+        Assert.state(strings != null && !strings.isEmpty()
+                && StringUtils.isNotBlank(strings.get(0)), "upload failed");
     }
 
 }
